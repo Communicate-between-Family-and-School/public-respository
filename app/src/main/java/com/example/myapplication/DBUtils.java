d05bb967e5d3007f79de2a76cb4799f4c7aee92b
xihe <2806559889@qq.com>
2023-07-07 10:08:39 +0800

    删除了com.example.myapplication.login 删除了layout.login.xml 删除了com.example.myapplication.temp 删除了layout.activity_temp.xml com.example.myapplication.DBUtils更改了getConnection函数（把Ip拿到了全局变量），添加了关闭连接函数CloseConnection，和查询函数Read，删除了函数LoginById LoginActivity重新写了login_btn.setOnClickListener函数

--- app/src/main/java/com/example/myapplication/DBUtils.java
+++ app/src/main/java/com/example/myapplication/DBUtils.java
@@ -1,10 +1,5 @@
 package com.example.myapplication;
 
-import static com.example.myapplication.IDENTITY.IdentityType.LEADER;
-import static com.example.myapplication.IDENTITY.IdentityType.PARENT;
-import static com.example.myapplication.IDENTITY.IdentityType.STUDENT;
-import static com.example.myapplication.IDENTITY.IdentityType.TEACHER;
-
 import android.nfc.Tag;
 import android.util.Log;
 
@@ -24,137 +19,89 @@ public class DBUtils {
     private static String user = "root";// 用户名
     private static String password = "uestc2022!";// 密码
     private static String username = "android";//数据库名
-    private static Connection getConnection(){
+
+    private static String ip = "124.71.219.185";// 写成本机地址，不能写成localhost，同时手机和电脑连接的网络必须是同一个
+
+    public static Connection getConnection() {
         Connection connection = null;
-        try{
+        try {
             Class.forName(driver);// 动态加载类
-            String ip = "124.71.219.185";// 写成本机地址，不能写成localhost，同时手机和电脑连接的网络必须是同一个
             // 尝试建立到给定数据库URL的连接
             int port = 3306;
             String url = "jdbc:mysql://" + ip + ":" + port
-                            + "/" + username + "?useUnicode=true&characterEncoding=utf-8&useSSL=false&allowPublicKeyRetrieval=true";
+                    + "/" + username + "?useUnicode=true&characterEncoding=utf-8&useSSL=false&allowPublicKeyRetrieval=true";
             connection = DriverManager.getConnection(url, user, password);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        if (connection == null) Log.e("DBUtils", "数据库连接失败");
         return connection;
     }
 
-    public static HashMap<String, Object> getInfoByName(String name){
+    public static void CloseConnection(Connection connection){
+        try{
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Log.e("DBUtils", "异常：" + e.getMessage());
+        }
+    }
+
+    public static ResultSet Read(PreparedStatement ps, Connection connection) {
+        ResultSet resultSet = null;
+        try {
+            if (connection != null) {// connection不为null表示与数据库建立了连接
+                if (ps != null) {
+                    resultSet = ps.executeQuery();
+                    if (resultSet == null)
+                        Log.e("DBUtils", "1.查询条件不满足\n2.数据库中无匹配数据\n3.查询语句错误");
+                } else Log.e("DBUtils", "1.SQL语句错误\n2.连接断开");
+            } else Log.e("DBUtils", "数据库连接失败");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("DBUtils", "异常：" + e.getMessage());
+        }
+        return resultSet;
+    }
+
+    public static HashMap<String, Object> getInfoByName(String name) {
         HashMap<String, Object> map = new HashMap<>();
         // 根据数据库名称，建立连接
         Connection connection = getConnection();
         try {
             String sql = "select * from student where name = ?";
-            if (connection != null){// connection不为null表示与数据库建立了连接
+            if (connection != null) {// connection不为null表示与数据库建立了连接
                 PreparedStatement ps = connection.prepareStatement(sql);
-                if (ps != null){
+                if (ps != null) {
                     // 设置上面的sql语句中的？的值为name
                     ps.setString(1, name);
                     // 执行sql查询语句并返回结果集
                     ResultSet rs = ps.executeQuery();
-                    if (rs != null){
+                    if (rs != null) {
                         int count = rs.getMetaData().getColumnCount();
-                        Log.d("DBUtils","列总数：" + count);
-                        while (rs.next()){
+                        Log.d("DBUtils", "列总数：" + count);
+                        while (rs.next()) {
                             // 注意：下标是从1开始的
-                            for (int i = 1;i <= count;i++){
+                            for (int i = 1; i <= count; i++) {
                                 String field = rs.getMetaData().getColumnName(i);
                                 map.put(field, rs.getString(field));
                             }
                         }
                         connection.close();
                         ps.close();
-                        return  map;
-                    }else Log.e("DBUtils","1.查询条件不满足\n2.数据库中无匹配数据\n3.查询语句错误");
-                }else Log.e("DBUtils","1.SQL语句错误\n2.连接断开");
-            }else Log.e("DBUtils","数据库连接失败");
+                        return map;
+                    } else
+                        Log.e("DBUtils", "1.查询条件不满足\n2.数据库中无匹配数据\n3.查询语句错误");
+                } else Log.e("DBUtils", "1.SQL语句错误\n2.连接断开");
+            } else Log.e("DBUtils", "数据库连接失败");
             return null;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            Log.e("DBUtils","异常：" + e.getMessage());
+            Log.e("DBUtils", "异常：" + e.getMessage());
             return null;
         }
-    }
-
-
-    public static IDENTITY LoginById(String account_text, String password_text) {
-        IDENTITY identity = new IDENTITY();
-        Connection connection = getConnection();
-        try {
-            String sql = "SELECT r.rname " +
-                    "FROM users u " +
-                    "JOIN user_role ur ON u.uid = ur.uid " +
-                    "JOIN role r ON ur.rid = r.rid " +
-                    "WHERE u.uid = ? " +
-                    "AND u.password = ?;";
-            if (connection != null){// connection不为null表示与数据库建立了连接
-                PreparedStatement ps = connection.prepareStatement(sql);
-                if (ps != null){
-                    ps.setString(1, account_text);
-                    ps.setString(2, password_text);
-                    ResultSet rs = ps.executeQuery();
-                    if (rs != null){
-                        if (rs.last()) {//int rowCount = 0;rowCount = rs.getRow();// 获取结果集的当前行数 rs.beforeFirst(); // 将结果集的指针重置到第一行之前
-                            String roleName = rs.getString("rname");
-                            if(roleName.equals("student")){
-                                identity.setIdentity(STUDENT);
-                            }
-                            if(roleName.equals("teacher")){
-                                identity.setIdentity(TEACHER);
-                            }
-                            if(roleName.equals("parent")){
-                                identity.setIdentity(PARENT);
-                            }
-                            if(roleName.equals("leader")){
-                                identity.setIdentity(LEADER);
-                            }
-                        }//Log.e("DBUtils","行总数：" + rowCount);
-                        ps.close();
-                    }else Log.e("DBUtils","1.查询条件不满足\n2.数据库中无匹配数据\n3.查询语句错误");
-                }else Log.e("DBUtils","1.SQL语句错误\n2.连接断开");
-            }else Log.e("DBUtils","数据库连接失败");
-        }catch (Exception e){
-            e.printStackTrace();
-            Log.e("DBUtils","异常：" + e.getMessage());
-        }
-        return identity;
-    }
-
-
-    public static ResultSet Read(String sql) {
-
-        Connection connection = getConnection();
-        try {
-            if (connection != null){// connection不为null表示与数据库建立了连接
-                PreparedStatement ps = connection.prepareStatement(sql);
-                if (ps != null){
-                    ResultSet rs = ps.executeQuery();
-                    if (rs != null){
-                        if (rs.last()) {//int rowCount = 0;rowCount = rs.getRow();// 获取结果集的当前行数 rs.beforeFirst(); // 将结果集的指针重置到第一行之前
-                            String roleName = rs.getString("rname");
-                        }
-                        ps.close();
-                    }else Log.e("DBUtils","1.查询条件不满足\n2.数据库中无匹配数据\n3.查询语句错误");
-                }else Log.e("DBUtils","1.SQL语句错误\n2.连接断开");
-            }else Log.e("DBUtils","数据库连接失败");
-        }catch (Exception e){
-            e.printStackTrace();
-            Log.e("DBUtils","异常：" + e.getMessage());
-        }
-
-//        Connection connection = getConnection();
-        ResultSet resultSet = null;
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            resultSet = ps.executeQuery();
-
-            if (resultSet != null)resultSet.close();
-            if (ps != null)ps.close();
-            if (connection != null)connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return resultSet;
     }
 }
+
+
+
--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,25 +1,36 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.IDENTITY.IdentityType.LEADER;
 import static com.example.myapplication.IDENTITY.IdentityType.NO_IDENTITY;
+import static com.example.myapplication.IDENTITY.IdentityType.PARENT;
+import static com.example.myapplication.IDENTITY.IdentityType.STUDENT;
+import static com.example.myapplication.IDENTITY.IdentityType.TEACHER;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginActivity extends AppCompatActivity {
     EditText account,password;//账号，密码
     Button login_btn,register_btn;//登录按钮，注册按钮
 
     private static final String Tag = "LoginActivity";
     private static String account_text = "",password_text = "";
-    private static IDENTITY identity = new IDENTITY(IDENTITY.IdentityType.STUDENT);
+    private static IDENTITY identity = new IDENTITY(NO_IDENTITY);
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
@@ -45,8 +56,6 @@ public class LoginActivity extends AppCompatActivity {
         account = findViewById(R.id.login_account);
         password = findViewById(R.id.login_password);
 
-
-
         login_btn = findViewById(R.id.login_login_btn);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
@@ -58,29 +67,57 @@ public class LoginActivity extends AppCompatActivity {
                         account_text = account.getText().toString();
                         password_text = password.getText().toString();
                         Message message = new Message();
-                        IDENTITY login_type = DBUtils.LoginById(account_text, password_text);
-                        if(login_type.getCurrentIdentity() != NO_IDENTITY){
-                            Intent nextMenuIntent = null;
 
-                            switch(login_type.getCurrentIdentity()){
-                                case STUDENT:
-                                    nextMenuIntent = new Intent(LoginActivity.this, StudentMenuActivity.class);
-                                    break;
-                                case TEACHER:
-                                    nextMenuIntent = new Intent(LoginActivity.this, TeacherMenuActivity.class);
-                                    break;
-                                case PARENT:
-                                case LEADER:
+                        String sql = "SELECT r.rname " +
+                                "FROM users u " +
+                                "JOIN user_role ur ON u.uid = ur.uid " +
+                                "JOIN role r ON ur.rid = r.rid " +
+                                "WHERE u.uid = ? " +
+                                "AND u.password = ?;";
+                        try {
+                            Connection connection = DBUtils.getConnection();
+                            PreparedStatement ps = connection.prepareStatement(sql);
+                            if (ps != null) {
+                                ps.setString(1, account_text);
+                                ps.setString(2, password_text);
+                                ResultSet rs = DBUtils.Read(ps,connection);
+                                if (rs != null) {
+                                    if (rs.last()) {
+                                        switch(rs.getString("rname")){
+                                            case "student":identity.setIdentity(STUDENT);break;
+                                            case "teacher":identity.setIdentity(TEACHER);break;
+                                            case "parent":identity.setIdentity(PARENT);break;
+                                            case "leader":identity.setIdentity(LEADER);break;
+                                        }
+                                    }
+                                    ps.close();
+                                }
                             }
-                            if(nextMenuIntent != null){
-                                nextMenuIntent.putExtra("account",account_text);
-                                startActivity(nextMenuIntent);
+                            DBUtils.CloseConnection(connection);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            Log.e("DBUtils", "异常：" + e.getMessage());
+                        }
 
-                                message.what = 1;
-                                handler.sendMessage(message);
-
-                                finish(); // 结束当前登录界面，防止返回到登录界面
-                            }
+                        Intent nextMenuIntent = null;
+                        switch(identity.getCurrentIdentity()){
+                            case STUDENT:
+                                nextMenuIntent = new Intent(LoginActivity.this, StudentMenuActivity.class);
+                                break;
+                            case TEACHER:
+                                nextMenuIntent = new Intent(LoginActivity.this, TeacherMenuActivity.class);
+                                break;
+                            case PARENT:
+                                break;
+                            case LEADER:
+                                break;
+                        }
+                        if(nextMenuIntent != null){
+                            nextMenuIntent.putExtra("account",account_text);
+                            startActivity(nextMenuIntent);
+                            message.what = 1;
+                            handler.sendMessage(message);
+                            finish(); // 结束当前登录界面，防止返回到登录界面
                         }else{
                             message.what = 2;
                             handler.sendMessage(message);
