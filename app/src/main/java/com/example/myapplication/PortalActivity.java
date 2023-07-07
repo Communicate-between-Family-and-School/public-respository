1a36fb11249e16ae05e2953ab2cb57c54c38ec97
PM2496 <2496288505@qq.com>
2023-07-07 11:02:35 +0800

    修复重复引用bug

--- app/src/main/java/com/example/myapplication/PortalActivity.java
+++ app/src/main/java/com/example/myapplication/PortalActivity.java
@@ -1,26 +1,26 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 public class PortalActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_portal);
+        TextView intro = (TextView) findViewById(R.id.introduction);
+        intro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(PortalActivity.this, IntroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
